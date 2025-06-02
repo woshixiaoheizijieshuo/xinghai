b8039f6b949499853ff937f919b57a33dbd83a75
woshixiaoheizijieshuo <dongruoqi2023@163.com>
2025-06-03 02:13:05 +0800

    Add files via upload

---
+++ 星海脚本.lua
@@ -0,0 +1,14 @@
+function Notify(Title1, Text1, Icon1, Time1)
+  game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = Title1,
+    Text = Text1,
+    Icon = Icon1,
+    Duration = Time1,
+  })
+end
+Notify("星海脚本", "作者:小鸟游星野", "rbxassetid://17360377302", 3)
+Notify("永久免费", "拒绝圈钱","rbxassetid://17360377302",3)
+Notify("星海脚本", "永久免费","rbxassetid://17360377302",3)
+Notify("抵制圈钱", "拒绝倒卖","rbxassetid://17360377302",3)
+Notify("呜嘿～", "新脚本请大家多多支持","rbxassetid:/17360377302",3
+Notify("加载好啦", "祝你游戏愉快！","rbxassetid://17360377302",5)
